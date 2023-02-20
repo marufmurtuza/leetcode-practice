c6ece3426ab3362cc4260575fdcb4773dce01987
Maruf Bin Murtuza <78444913+marufmurtuza@users.noreply.github.com>
2023-02-20 10:25:00 +0600

    Add files via upload

--- (Problem-1491)-Average Salary Excluding the Minimum and Maximum Salary.cpp
+++ (Problem-1491)-Average Salary Excluding the Minimum and Maximum Salary.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     double average(vector<int>& salary) {
-        sort(salary.begin(), salary.end());
-        double sum, average;
-        for(int i = 1; i < salary.size()-1; i++){
-            sum = sum + salary[i];
+        int salsum = 0;
+        int salavg = 0;
+        sort(salary.begin(),salary.end());
+        salary.pop_back();       
+        for(int i = 1; i < salary.size(); i++){
+            salsum += salary[i];
         }
-        average = sum/(salary.size()-2);
-        return average;       
+        salavg = salsum/(salary.size()-1);
+        return salavg;
     }
 };
